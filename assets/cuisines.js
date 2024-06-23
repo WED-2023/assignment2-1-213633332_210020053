5a1eaccc8ac04eaff0cc4e9423baa548b914d2c2
aedanspellsword <145868860+IdanHefetzBenGurion@users.noreply.github.com>
2024-06-23 22:08:35 +0300

    Delete assets directory

--- assets/cuisines.js
+++
@@ -1,8 +0,0 @@
-export default [
-    'African', 'Asian', 'American', 'British', 'Cajun', 'Caribbean', 'Chinese', 
-    'Eastern European', 'European', 'French', 'German', 'Greek', 'Indian', 
-    'Irish', 'Italian', 'Japanese', 'Jewish', 'Korean', 'Latin American', 
-    'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'Southern', 
-    'Spanish', 'Thai', 'Vietnamese'
-  ];
-  
