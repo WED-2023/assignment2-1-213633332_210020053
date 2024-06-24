a23a133680d8b52ab7b2e0c46da537c76c756f81
aedanspellsword <145868860+IdanHefetzBenGurion@users.noreply.github.com>
2024-06-24 17:15:34 +0300

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,5 @@ module.exports = {
     // host: "132.72.65.211"
     host: "localhost"
   }
-  publicPath: process.env.NODE_ENV === "production" ? "/assignment2-1-213633332_210020053/" : "/"
+  // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
 };
